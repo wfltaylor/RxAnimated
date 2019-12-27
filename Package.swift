5efd9af538992aa633cf0df9502f86c37948f1c4
wfltaylor <wfltaylor@gmail.com>
2019-12-27 09:17:47 +0100

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version:5.0
+//
+//  SnapKit
+//
+//  Copyright (c) 2011-Present SnapKit Team - https://github.com/SnapKit
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+
+import PackageDescription
+
+let package = Package(
+    name: "RxAnimated",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(name: "RxAnimated", targets: ["RxAnimated"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
+    ],
+    targets: [
+        .target(name: "RxAnimated", dependencies: ["RxSwift", "RxCocoa"], path: "RxAnimated/Core")
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]
+)
